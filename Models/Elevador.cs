4b14d6469321bcb156aea6f2f8e55a3a911623a7
Caio Souza Queiroz <71792216+caiousouza@users.noreply.github.com>
2022-05-28 20:22:24 -0300

    Add files via upload

---
+++ Models/Elevador.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoelevador.Model
+{
+    public class Elevador
+    {
+        //A proproedade Get-Set será utilizada para ler e atribuir um novo valor para as variaveis.
+
+        public int PessoasAtual { get; set; } //Variavel que armazenara a quantidade atual de passageiros.
+        public int AndarAtual { get; set; } //Variael que armazenara a posição do atual do elevador.
+        public int PessoasTotal { get; set; } //variavel que irá armazenar a capacidade Maxima de passageiros.
+        public int AndarTotal { get; set; } //Variavel que irá armazenar a quantidade de andares do elevador.
+
+        public int andarTotal; //Variavel que armazenará a decisão do user
+        public int pessoasTotal; //Variavel que armazenará a decisão do user.
+
+        public Elevador()
+        {
+            PessoasAtual = 0; // é dado o valor "0" para começar vazio.
+            AndarAtual = 0; //É dado o valor "0" para começar no terio.
+        }
+
+        public void Inicializar() //Metodo que Iniciará o programa, perguntando do user a quantidade de andares e a capacidade do elevador.
+        {
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Digite a Quantidade de ANDARES que o predio possui");
+            andarTotal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade MAXIMA de Passageiros para o elevador");
+            pessoasTotal = Convert.ToInt32(Console.ReadLine());
+
+            //As variaveis "andarTotal" e "pessoasTotal" serão transformadas em variaveis inteiras.
+
+            AndarTotal = andarTotal; //A variavel "AndarTotal" irá armazenar a variavel "andarTotal".
+            PessoasTotal = pessoasTotal; //A variavel "PessoasTotal" irá armazenar a variael "pessoasTotal".
+        }
+
+        public string Entrar() //Metodo de será utilizado para adicionar +1 passageiro.
+        {
+            if (PessoasAtual < PessoasTotal) //Se a quantidade de passageiros for menor que a capacidade maxima, será adicionado mais um passageiro.
+            {
+                PessoasAtual++;  //Para acrescentar mais um passageiro.
+                if (AndarAtual == 0) 
+                {
+                    Console.Clear();
+                    return "O elevador está no terrio, e tem " + PessoasAtual + " pessoa(s) no elevador";  //Se for adicionado 1 passageiro, mas o elevador ainda permanece no terrio, ele retornará a mensagem.
+                }
+
+                Console.Clear();
+
+                return "Entrou uma pessoa no elevador, o elevador está no " + AndarAtual + "° andar, e tem " + PessoasAtual + " pessoa(s) no elevador";  //Mensagem para atualizar as informaçõse do elevador.
+            }
+            else
+            {
+                Console.Clear();
+
+                return "O elevador atingiu a capacidade maxima, o elevador está " + AndarAtual + "° andar, e tem " + PessoasAtual + "pessoa(s) no elevador";// Se a capacidade maximo do elevador foi atingida, ele retornará a mensagem.
+            }
+        }
+
+        public string Sair() //Metodo que será utilizado para subitrair um passageiro.
+        {
+            if (PessoasAtual > 0) //Só será possivel realizar a operção se houver algum passageiro no elevador.
+            {
+                PessoasAtual--; //para subitrair 1 da variavel.
+
+                Console.Clear();
+
+                return "Saiu uma pessoa do elevador, o elevador está no " + AndarAtual + "° andar, e tem " + PessoasAtual + " pessoa(s) no elevador"; //Informara que saiu um passageiro, e dará as informações do elevador.
+            }
+            else
+            {
+                Console.Clear();
+
+                return "O elevador está vazio, o elevador está " + AndarAtual + " andar, e tem " + PessoasAtual + " pessoa(s) no elevador"; //Informara que não o elevador está vazio, e não poderar realizar a decisão escolida.
+            }
+        }
+
+        public string subir() //Metodo que será utilizado para subir um andar por vez.
+        {
+            if (AndarAtual < AndarTotal) //Se o andar atual for menor que o limite de andares, então ele subirar +1.
+
+            {
+                AndarAtual++; //Para adicionar 1 a variavel
+
+                Console.Clear();
+
+                return "O elevador subiu um andar, o elevador está no " + AndarAtual + "° andar, e tem " + PessoasAtual + " pessoa(s) no elevador"; //Mensagem para informar que o elevador subiu um andar, e atualizar as informações do elevador.
+            }
+            else
+            {
+                Console.Clear();
+
+                return "O elevador não pode Subir mais que a cobertura, o elevador está no " + AndarAtual + "° andar, e tem " + PessoasAtual + " pessoa(s) no elevador"; //Mensagem para informar que não é permitido subir mais que a quantidade escolida anteriormente.
+            }
+        }
+
+        public string Descer() //Metodo para descer um andar por vez.
+        {
+            if (AndarAtual > 0) //Se o andar selecionado for acima do terrio, ele descerá mais um andar.
+            {
+                AndarAtual--; //Para subitrair 1 da variavel.
+
+
+                Console.Clear();
+
+                return "O elevador desceu um andar, o elevador está no " + AndarAtual + "° andar, e tem " + PessoasAtual + " pessoa(s) no elevador"; //Mensagem para informar que o elevador desceu um andar, com o seu posicionamento atual e a quantidade de passageiros,
+            }
+            else
+            {
+                Console.Clear();
+
+                return "O elevador não pode descer mais que o Terrio, o elevador está no " + AndarAtual + "° andar, e tem " + PessoasAtual + " pessoa(s) no elevador"; //Mensagem para informar que não é possivel descer mais que o terrio.
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using projetoelevador.Model;
+
+namespace projetoelevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevador elevador = new Elevador(); //A classe "elevador" irá receber um nova posição do elevador.
+
+            int Num = 0; //Variavel que irá receber a variavel "escolha".
+            string Escolha; //variavel que irá receber a decisão do user.
+            string decisao; //Variavel que receberá as Funções.
+
+            Console.WriteLine("Bem Vindo a Ocean Tower");
+
+            elevador.Inicializar(); //Metodo "Incializar" será utilizador logo no inicio do programa para pergunar e armazanar a quantidade de andares e passageiros. 
+
+            do
+            {
+                Console.WriteLine("\n");
+                //Escolhas disponiveis que o user pode optar por usar.
+                Console.WriteLine(@"
+                1° - Aperte [1] para 'ADICIONAR' uma pessoa no elevador;
+                2° - Aperte [2] para 'RETIRAR' uma pessoa do elevador;
+                3° - Aperte [3] para 'SUBIR' um andar no predio;
+                4° - Aperte [4] para 'DESCER' um andar no predio;
+                5° - Aperte [5] para 'FINALIZAR' o programa;
+                ");
+
+                Escolha = Console.ReadLine(); //A variavel "Escolha" irá receber a decisão tomada pelo user.
+
+                Num = Convert.ToInt32(Escolha); //A variavel "escolha" será convertida em inteira, e será armazenada dentro da variavel "Num".
+
+
+                switch (Num) //O Switch írá devolver a escolha do user, trazendo os metodos disponiveis na classe Elevador.
+                {
+                    case 1: decisao = elevador.Entrar(); Console.WriteLine(decisao); break;
+                    case 2: decisao = elevador.Sair(); Console.WriteLine(decisao); break; 
+                    case 3: decisao = elevador.subir(); Console.WriteLine(decisao); break; 
+                    case 4: decisao = elevador.Descer(); Console.WriteLine(decisao); break;
+                    case 5: Console.WriteLine("Fim do programa"); break;
+                    default: Console.WriteLine("\nOPÇÃO INVALIDA\n"); break;  //Caso o user não escolha uma odas opções cidadas acima.
+
+                }
+            }
+            while (Num != 5); // Caso a decisão de Finalizar o programa for escolida, o laço de repetição irá encerrar o ciclo.
+            {
+
+            }
+        }
+    }
+}
